28d779a7fe84ad83215fd0086e04a6e63c34d3a2
bondkirik <61831633+bondkirik@users.noreply.github.com>
2022-08-02 18:46:37 +0300

    Get all ShopItemsRepository for shopList

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ShopItemsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,10 +22,13 @@ class IndexController extends AbstractController
     /**
      * @Route("/shop/list", name="shopList")
      */
-    public function shopList(): Response
+    public function shopList(ShopItemsRepository $itemsRepository): Response
     {
+        $items = $itemsRepository->findAll();
+
         return $this->render('index/shopList.html.twig', [
             'title' => 'SHOP LIST',
+            'items' => $items,
         ]);
     }
 
